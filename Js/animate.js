b4fc2848749ed683b72d5f763338dd19c818ee3d
Janemon <janemon2331@gmail.com>
2019-01-17 18:15:35 +0800

    Document.creatElement

--- Js/animate.js
+++ Js/animate.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
   setTimeout(()=>{
     let h1=document.getElementsByTagName("h1")[0]
-    let h2=document.createElement("<h2>Hi!! I am Janemon.</h2>")
+    let h2=Document.createElement("<h2>Hi!! I am Janemon.</h2>")
     h1.appendChild(h2)
   }, 2000)
 })
